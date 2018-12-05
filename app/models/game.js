4245f844659aa2540f4e7aeb5a8572bf523ce9d8
Victor Guadarrama <victor.gm@eucledian.com>
2018-12-05 11:30:01 -0600

    Games Results v1

--- app/models/game.js
+++ app/models/game.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   homeTeam: DS.belongsTo('team', { inverse: 'homeGames'}),
   awayTeam: DS.belongsTo('team', { inverse: 'awayGames'}),
-  homeGoal: DS.attr('number'),
-  awayGoal: DS.attr('number'),
+  homeGoals: DS.attr('number'),
+  awayGoals: DS.attr('number'),
   playedOn: DS.attr('date')
 });
--- app/services/game-simulator.js
+++ app/services/game-simulator.js
@@ -1,24 +1,87 @@
 import Service from '@ember/service';
 import { inject } from '@ember/service';
+import { later } from '@ember/runloop';
+import { shuffle } from 'ember-composable-helpers/helpers/shuffle';
+import { computed } from '@ember/object';
+
+let DELAY_BETWEEN_GAMES = 1000;
 
 export default Service.extend({
   store: inject(),
 
+  teams: computed(function() {
+    return this.store.peekAll('team');
+  }),
+
+  games: computed(function() {
+    return this.store.peekAll('game');
+  }),
+
   init() {
     this._super(...arguments);
 
-    //console.log('Game sim...');
-
     this.seedTeams();
 
+    later(this, this.simulateGame, DELAY_BETWEEN_GAMES);
   },
 
   seedTeams() {
-    let teamNames = ['Team 1', 'Team 2', 'Team 3', 'Team 4'];
+    let teamNames = [
+      'Liverpool',
+      'Chelsea',
+      'Manchester City',
+      'Arsenal',
+      'Tottenham',
+      'Bournmouth',
+      'Watford',
+      'Manchester United',
+      'Everton',
+      'Wolves',
+      'Brighton',
+      'Leicester City',
+      'West Ham',
+      'Crystal Palace',
+      'Burnley',
+      'Southamptom',
+      'Cardiff City',
+      'Fulham',
+      'Newcastle United',
+      'Huddersfield Town'
+    ];
 
     for(let i=0; i<teamNames.length; i++) {
       this.store.createRecord('team', { id: i, name: teamNames[i] });
     }
+  },
+
+  simulateGame() {
+    let teams = this.store.peekAll('team');
+
+    let shuffledTeams = shuffle(teams);
+
+    let homeTeam = shuffledTeams[0];
+    let awayTeam = shuffledTeams[1];
+
+    let homeGoals = this.randomScore(4);
+    let awayGoals = this.randomScore(3);
+
+    //console.log({ homeGoals, awayGoals });
+
+    this.store.createRecord('game', {
+      homeTeam,
+      awayTeam,
+      homeGoals,
+      awayGoals,
+      playedDate: new Date()
+    });
+
+    later(this, this.simulateGame, DELAY_BETWEEN_GAMES);
+
+  },
+
+  randomScore(maximumGoals) {
+    return Math.round((Math.random() * maximumGoals))
   }
+
 
 });
