9f3213d70633ce14b5cca06916fed797d88d404c
Victor Guadarrama <victor.gm@eucledian.com>
2018-12-06 19:00:39 -0600

    League table first part

--- app/models/game.js
+++ app/models/game.js
@@ -1,9 +1,24 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 
 export default DS.Model.extend({
   homeTeam: DS.belongsTo('team', { inverse: 'homeGames'}),
   awayTeam: DS.belongsTo('team', { inverse: 'awayGames'}),
   homeGoals: DS.attr('number'),
   awayGoals: DS.attr('number'),
-  playedOn: DS.attr('date')
+  playedOn: DS.attr('date'),
+
+  isDraw: computed('homeGoals', 'awayGoals', function() {
+    return this.homeGoals === this.awayGoals;
+  }),
+
+  isHomeWin: computed('homeGoals', 'awayGoals', function() {
+    return this.homeGoals > this.awayGoals;
+  }),
+
+  isAwayWin: computed('homeGoals', 'awayGoals', function() {
+    return this.homeGoals < this.awayGoals;
+  }),
+
+
 });
--- app/models/team.js
+++ app/models/team.js
@@ -1,7 +1,20 @@
 import DS from 'ember-data';
+import { union, filterBy, sum, mapBy } from '@ember/object/computed';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   homeGames: DS.hasMany('game', { inverse: 'homeTeam' }),
   awayGames: DS.hasMany('game', { inverse: 'awayTeam' }),
+
+  games: union('homeGames', 'awayGames'),
+
+  homeGamesWon: filterBy('homeGames', 'isHomeWin'),
+  awayGamesWon: filterBy('awayGames', 'isAwayWin'),
+  gamesWon: union('homeGamesWon', 'awayGamesWon'),
+
+  homeGamesLost: filterBy('homeGames', 'isAwayWin'),
+  awayGamesLost: filterBy('awayGames', 'isHomeWin'),
+  gamesLost: union('homeGamesLost', 'awayGamesLost'),
+
+  gamesDrawn: filterBy('games', 'isDraw'),
 });
